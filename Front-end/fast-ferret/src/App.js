cad1cd547ea99a9c20704a6130e4b21bb778ae25
qwertyAscii <crisshome74@gmail.com>
2021-08-15 02:29:09 +0300

    Add index page

---
+++ Front-end/fast-ferret/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body className="App-body">
+        <searchcontainer className="Search">
+          Fastferret
+          <spacer className="Spacer"></spacer>
+          <form action="asd">
+            <input type="text" className="Search-bar"></input>
+          </form>
+        </searchcontainer>
+      </body>
+    </div>
+  );
+}
+
+export default App;
